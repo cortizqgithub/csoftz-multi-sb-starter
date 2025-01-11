c68adfbcaeec6625f9144969b89358a0a8ecab4f
Carlos Adolfo Ortiz Quiros (Disney) <Carlos.Y.Ortiz.-ND@disney.com>
2025-01-10 20:32:02 -0500

    Add Initial files.

---
+++ src/main/kotlin/com/csoftz/multi/sb/starter/CsoftzMultiSbStarterApplication.kt
@@ -0,0 +1,11 @@
+package com.csoftz.multi.sb.starter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CsoftzMultiSbStarterApplication
+
+fun main(args: Array<String>) {
+    runApplication<CsoftzMultiSbStarterApplication>(*args)
+}
---
+++ src/test/kotlin/com/csoftz/multi/sb/starter/CsoftzMultiSbStarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.csoftz.multi.sb.starter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CsoftzMultiSbStarterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
